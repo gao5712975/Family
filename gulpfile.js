6de60b19b2328579dd1542c2e50e620a392beb04
moka <gaoyuanfell@sina.com>
2016-07-16 21:11:02 +0800

    没心情

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Yuan on 2016/7/16.
+ */
+var gulp = require('gulp');
+var del = require('del'); //文件删除
+var runSequence = require('run-sequence'); //异步任务
+var gulpWatch = require('gulp-watch'); //监听插件
+var uglify = require('gulp-uglify');
